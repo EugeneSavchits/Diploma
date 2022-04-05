afaa7c216348e22b59cbb8fb36173cb849257121
Lenovo <sea20@mail.com>
2022-04-06 02:36:17 +0300

    HW

--- src/main/java/utils/Listener.java
+++ src/main/java/utils/Listener.java
@@ -13,37 +13,22 @@ import java.io.ByteArrayInputStream;
 
 public class Listener implements ITestListener {
 
-//    @Override
-//    public void onTestFailure(ITestResult tr) {
-//        Object currentClass = tr.getInstance();
-//        System.out.println("Делаю скриншот!!!!!!");
-//        try {
-//            WebDriver driver = ((BaseTest) currentClass).driver;
-//            byte[] srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-//            saveScreenshot(srcFile);
-//        } catch (Exception ex) {
-//
-//        }
-//    }
-
     @Override
-    public void onTestFailure(ITestResult result) {
+    public void onTestFailure(ITestResult tr) {
+        Object currentClass = tr.getInstance();
+        try {
+            WebDriver driver = ((BaseTest) currentClass).driver;
+            byte[] srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            saveScreenshot(srcFile);
+        } catch (Exception ex) {
 
-        Allure.addAttachment("screenShot", new ByteArrayInputStream(((TakesScreenshot)result.getTestContext().getAttribute("WebDriver")).getScreenshotAs(OutputType.BYTES)));
-
-        Object webDriverAttribute = result.getTestContext().getAttribute("WebDriver");
-        captureScreenshot((WebDriver) webDriverAttribute);
-
+        }
     }
 
 
-    @Attachment
-    public byte[] captureScreenshot(WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-    }
 
-//    @Attachment(value = "Page screenshot", type = "image/png")
-//    private byte[] saveScreenshot(byte[] screenshot) {
-//        return screenshot;
-//    }
+    @Attachment(value = "Page screenshot", type = "image/png")
+    private byte[] saveScreenshot(byte[] screenshot) {
+        return screenshot;
+    }
 }
