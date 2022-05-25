523b49c26cdf53932e31d041e320bb5ffd2e51a1
Lenovo <sea20@mail.com>
2022-05-25 18:02:25 +0300

    HW

--- src/test/java/tests/LoginNegativeScreenshotTest.java
+++ src/test/java/tests/LoginNegativeScreenshotTest.java
@@ -8,23 +8,22 @@ import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
 
-//public class LoginNegativeScreenshotTest extends BaseTest {
-//
-//
-//    @Test
-//    public void loginNegativeScreenshotTest () {
-//        User user = new User();
-//        user.setEmail(ReadProperties.getUsername());
-//        user.setPassword(System.getProperty("PASSWORD"));
-//
-//        LoginPage loginPage = new LoginPage(driver);
-//        loginPage.login(user);
-//
-//        DashboardPage dashboardPage = new DashboardPage(driver);
-//
-//        Assert.assertFalse(dashboardPage.getAddProjectButton().isDisplayed());
-//    }
-//
-//}
+public class LoginNegativeScreenshotTest extends BaseTest {
+
+
+    @Test
+    public void loginNegativeScreenshotTest () {
+        User user = new User();
+        user.setEmail(ReadProperties.getUsername());
+        user.setPassword(System.getProperty("PASSWORD"));
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(user);
+
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+        Assert.assertFalse(dashboardPage.getAddProjectButton().isDisplayed());
+    }
+}
 
 
